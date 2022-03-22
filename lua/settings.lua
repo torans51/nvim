958a1e11d19f145b6f6815b091e07cb5d6902fe9
torans51 <>
2022-03-22 16:42:35 +0100

    remove unused or useless settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,23 +1,13 @@
 local cmd = vim.cmd
 local opt = vim.opt
 
-cmd [[
-  syntax enable
-  filetype on
-  filetype plugin on
-]]
 
 local options = {
-  hidden = true,                           -- required to keep multiple buffers
-  encoding = 'utf-8',                      -- the encoding displayed
-  fileencoding = "utf-8",                  -- the encoding written to a file
   clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
   mouse = "a",                             -- allow the mouse to be used in neovim
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   timeoutlen = 400,                        -- time to wait for a mapped sequence to complete (in milliseconds)
   updatetime = 300,                        -- faster completion (4000ms default)
-  showtabline = 2,                         -- always show tabs
-  hlsearch = true,                         -- highlight all matches on previous search pattern
   ignorecase = true,                       -- ignore case in search patterns
   smartcase = true,                        -- smart case
   smartindent = true,                      -- make indenting smarter again
